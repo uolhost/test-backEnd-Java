1ac056080aba50acf8ab473967f46bd7c77a46a9
Cleonildo Jr <cleu.junior@gmail.com>
2023-08-25 18:48:01 -0600

    feat: initial commit

---
+++ src/main/java/com/superherogame/cleonildo/SuperHeroGameApplication.java
@@ -0,0 +1,13 @@
+package com.superherogame.cleonildo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperHeroGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperHeroGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/superherogame/cleonildo/SuperHeroGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superherogame.cleonildo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperHeroGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
